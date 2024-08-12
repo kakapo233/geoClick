f48df82556306c0851f848ea4b2e70658025d777
Kakapo233 <101606510+kakapo233@users.noreply.github.com>
2024-08-13 09:43:12 +1200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+/* clicker game*/
+/* PT102 */
+/* Seamus Geoghegan */
+var currencyCount=0;
+var shapeValue=[3,4,5,6,7,8,9,10];
+var baseShapeCost=[20,300,5000,70000,110000,1300000,17000000,190000000];
+var shapeCostMultiplyer=1.15;
+var numOfTri=0;
+var numOfSqu=0;
+var numOfPen=0;
+var numOfHex=0;
+var numOfsep=0;
+var numOfOct=0;
+var numOfNon=0;
+var numOfDec=0;
+var idOfTri=0;
+var idOfSqu=1;
+var idOfPen=2;
+var idOfHex=3;
+var idOfsep=4;
+var idOfOct=5;
+var idOfNon=6;
+var idOfDec=7;
